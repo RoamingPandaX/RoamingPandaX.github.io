4cee9ee89cd4409973827f2efe51b12fd4e90b47
Vo Minh Hieu <vominhhieu24022002@gmail.com>
2022-10-31 00:00:12 +0700

    updates

--- script/main/recruit.js
+++ script/main/recruit.js
@@ -7,7 +7,7 @@ document.getElementById("btn-dropDownMenu").onclick = () => {
 }
 
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 30, 2022 23:59:59").getTime();
+var countDownDate = new Date("Oct 30, 2022 16:03:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -23,12 +23,20 @@ var x = setInterval(function() {
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="demo"
-  document.getElementById("day").innerHTML = days;
-  document.getElementById("hour").innerHTML = hours;
-  document.getElementById("min").innerHTML = minutes;
-  document.getElementById("second").innerHTML = seconds;
+
+  if(days>=0 && hours >=0 && minutes>=0 && seconds >=0) {
+    // Output the result in an element with id="demo"
+    document.getElementById("day").innerHTML = days;
+    document.getElementById("hour").innerHTML = hours;
+    document.getElementById("min").innerHTML = minutes;
+    document.getElementById("second").innerHTML = seconds;
+  } else {
+    document.getElementById("day").innerHTML = 0;
+    document.getElementById("hour").innerHTML = 0;
+    document.getElementById("min").innerHTML = 0;
+    document.getElementById("second").innerHTML = 0;
+  }
+   
     
   // If the count down is over, write some text 
 //   if (distance < 0) {
@@ -41,3 +49,11 @@ $(".num").countUp({
   time: 2000,
   delay: 10,
 });
+
+document.getElementsByClassName("apply-ngay")[0].onclick = () => {
+  document.getElementsByClassName("noti")[1].style.display = "block";
+}
+
+document.getElementsByClassName("dangKyNgay")[0].onclick = () => {
+  document.getElementsByClassName("noti")[0].style.display = "block";
+}
